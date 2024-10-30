38e317454834f429a1adbe385e3339240be31073
Git2Satyam <119624745+Git2Satyam@users.noreply.github.com>
2024-10-30 00:27:17 -0400

    Delete StudentFrom directory

--- StudentFrom/StudentFrom/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace StudentFrom.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
